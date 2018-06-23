b843118587fb317b812087d62d90f2ca0b01618a
Rafael Valdez <valdezr1@outlook.com>
2018-06-23 00:42:18 -0700

    solution attempted, but not to LC standards

--- Easy/118-Pascals-Triangle/valdezr1/solution.cpp
+++ Easy/118-Pascals-Triangle/valdezr1/solution.cpp
@@ -21,3 +21,40 @@
         This would take O(n) Time and Space Complexity...I think. 
 */
 
+// Seems like LC wants the solution to be returned as a vector of vectors (2d). 
+// Here, focusing primarily on functionality
+
+
+// n resembles the number of rows to be printed in pascals' triangle
+void printPascal(int n){
+    if (n <= 0){
+        return;
+    }
+    else if (n == 1){
+        cout << 1 << endl;
+        return;
+    }
+    else {
+        int result;
+        cout << 1 << endl;
+        cout << "1 1" << endl;
+        // Initialize two vectors
+        vector<int> prevVec = {1, 1};
+        vector<int> nextVec;
+        for(int i = 0; i < n; i++){
+            nextVec.push_back(1);
+            cout << 1;
+            for(int i = 0; i < prevVec.size() - 1; i++){
+                result = prevVec[i] + prevVec[i + 1];
+                nextVec.push_back(result);
+                cout << result;
+            }
+            nextVec.push_back(1);
+            cout << 1;
+
+            // Set prevVec to nextVec and clear nextVec
+            prevVec = nextVec;
+            nextVec = vector<int>();
+        } 
+    }
+}
